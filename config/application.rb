eece05c708420c6f381b61b580173fd9883fa2d2
ntqtuan-cmcglobal <ntqtuan@cmcglobal.vn>
2025-08-08 17:11:39 +0700

    fix(configuration): update Redis URL to use environment variable and set Active Storage service to production

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module Testapp
     # Sidekiq configuration
     config.active_job.queue_adapter = :sidekiq
 
-    config.cache_store = :redis_cache_store, { url: 'redis://redis:6379/0', expires_in: 90.minutes }
+    config.cache_store = :redis_cache_store,
+                         { url: ENV.fetch('REDISCLOUD_URL', 'redis://redis:6379/0'), expires_in: 90.minutes }
 
     config.rack_mini_profiler_environments = %w[development test]
 
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,9 @@ require 'sidekiq'
 require 'sidekiq-scheduler'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: ENV.fetch('REDISCLOUD_URL', 'redis://redis:6379/0') }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: ENV.fetch('REDISCLOUD_URL', 'redis://redis:6379/0') }
 end
