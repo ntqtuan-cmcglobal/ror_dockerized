fae0e6adc97c1f65cafc4e8736e898588b63bbb1
ntqtuan-cmcglobal <ntqtuan@cmcglobal.vn>
2025-08-15 17:39:42 +0700

    update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,8 +3,10 @@ require 'sidekiq-scheduler'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDISCLOUD_URL', 'redis://redis:6379/0') }
+  config.logger.level = Logger::DEBUG
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDISCLOUD_URL', 'redis://redis:6379/0') }
+  config.logger.level = Logger::DEBUG
 end
